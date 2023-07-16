945755fd9209e2b6ab0b720e698882c34c6023c6
eliitApple <eliit.apple@gmail.com>
2023-07-16 13:56:13 +0200

    Write class to fix exceptions

--- src/main/java/com/serenitydojo/exceptions/WordCounter.java
+++ src/main/java/com/serenitydojo/exceptions/WordCounter.java
@@ -1,2 +1,12 @@
-package com.serenitydojo.exceptions;public class WordCounter {
+package com.serenitydojo.exceptions;
+
+public class WordCounter {
+
+    public int numberOfWordsIn(String someString) {
+        if (someString == null) {
+            return 0;
+        }
+        int numberOfWords = someString.split("\\W").length;
+        return numberOfWords;
+    }
 }
