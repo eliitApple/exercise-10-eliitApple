d13177069244869338128af7930ec15b5682f918
eliitApple <eliit.apple@gmail.com>
2023-07-16 14:36:26 +0200

    Add custom exception

--- src/main/java/com/serenitydojo/exceptions/WhenWorkingWithExceptions.java
+++ src/main/java/com/serenitydojo/exceptions/WhenWorkingWithExceptions.java
@@ -2,24 +2,46 @@ package com.serenitydojo.exceptions;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class WhenWorkingWithExceptions {
+
+    WordCounter wordCounter = new WordCounter();
     @Test
     public void shouldCountWordsInAString() {
-        WordCounter wordCounter = new WordCounter();
+
 
         int numberOfWords = wordCounter.numberOfWordsIn("some string");
 
         assertThat(numberOfWords).isEqualTo(2);
-
     }
     @Test
     public void shouldReturnZeroForANullString() {
-        WordCounter wordCounter = new WordCounter();
+
 
         int numberOfWords = wordCounter.numberOfWordsIn(null);
 
         assertThat(numberOfWords).isEqualTo(0);
     }
+
+    @Test
+    public void shouldCountWordsInAFile() throws IOException, FileHasNoWordsException {
+
+
+        int numberOfWords = wordCounter.numberOfWordsInFile("src/main/resources/hello.txt");
+    }
+
+    @Test(expected = FileHasNoWordsException.class)
+    public void shouldReportAnErrorIfTheFileDoesNotExist() throws  IOException, FileHasNoWordsException{
+        int numberOfWords = wordCounter.numberOfWordsInFile("file-that-doesn't-not-exist.txt");
+        assertThat(numberOfWords).isEqualTo(0);
+    }
+
+    @Test(expected = FileHasNoWordsException.class)
+    public void shouldThrowMeaningfulExceptionIfThereAreNoWordsInTheFile() throws IOException, FileHasNoWordsException {
+        wordCounter.numberOfWordsInFile("src/main/resources/no_words.txt");
+    }
+
 }
--- src/main/java/com/serenitydojo/exceptions/WordCounter.java
+++ src/main/java/com/serenitydojo/exceptions/WordCounter.java
@@ -1,5 +1,10 @@
 package com.serenitydojo.exceptions;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+
 public class WordCounter {
 
     public int numberOfWordsIn(String someString) {
@@ -8,5 +13,18 @@ public class WordCounter {
         }
         int numberOfWords = someString.split("\\W").length;
         return numberOfWords;
+    }
+
+    public int numberOfWordsInFile(String filename) throws IOException, FileHasNoWordsException {
+        try {
+            String fileContents = Files.readString(Paths.get(filename));
+            int wordCount = numberOfWordsIn(fileContents);
+            if(wordCount == 0) {
+                throw new FileHasNoWordsException("No words in the file " + filename);
+            }
+            return wordCount;
+        } catch (NoSuchFileException noSuchFile) {
+            throw new FileHasNoWordsException("No words in the non existent file " + filename);
+        }
     }
 }
